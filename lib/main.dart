893ce4ab3047b2fd9bbc52e71330500efb21f8ba
iqballiii <meet.iqbal11@gmail.com>
2022-09-07 13:58:24 +0530

    pdf testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:about_me/pdf_export.dart';
+import 'package:pdf/pdf.dart' as pw;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pdf/widgets.dart' as pdw;
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'dart:io';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,6 +16,89 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<void> securePdf() async {
+    PdfDocument document = PdfDocument(inputBytes: await makePdf());
+    document.security.userPassword = '123456789';
+    document.security.ownerPassword = 'owner@123';
+    List<int> bytes = await document.save();
+    document.dispose();
+    print('The save file function is called');
+
+    _launchPdf(bytes, 'secured.pdf');
+  }
+
+  Future<List<int>> _readDocumentData(String name) async {
+    final ByteData data = await rootBundle.load('assets/$name');
+    return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  }
+
+  Future<void> _launchPdf(List<int> bytes, String fileName) async {
+    Directory? directory = await getExternalStorageDirectory();
+    String path = directory!.path;
+    File file = File('$path/$fileName');
+    await file.writeAsBytes(bytes, flush: true);
+    //saves the document.
+    _savePDF(fileName, bytes);
+    print('The save file function is called');
+
+    OpenFile.open('$path/$fileName');
+  }
+
+  Future<void> restrictPermissions() async {
+    //Load the existing PDF document.
+    PdfDocument document =
+        PdfDocument(inputBytes: await _readDocumentData('test_data.pdf'));
+    //Create document security.
+    PdfSecurity security = document.security;
+    //Set the owner password for the document.
+    security.ownerPassword = 'owner@123';
+    //Set various permission.
+    security.permissions.addAll(<PdfPermissionsFlags>[
+      PdfPermissionsFlags.fullQualityPrint,
+      PdfPermissionsFlags.print,
+      PdfPermissionsFlags.fillFields,
+      PdfPermissionsFlags.copyContent
+    ]);
+    //Save and dispose the document.
+    List<int> bytes = await document.save();
+    document.dispose();
+    //Open the PDF file.
+    _launchPdf(bytes, 'permissions.pdf');
+  }
+
+  Future<void> decryptPDF() async {
+    //Load the PDF document with permission password.
+    PdfDocument document = PdfDocument(
+        inputBytes: await _readDocumentData('secured.pdf'),
+        password: 'owner@123');
+    //Get the document security.
+    PdfSecurity security = document.security;
+    //Set owner and user passwords are empty string.
+    security.userPassword = '';
+    security.ownerPassword = '';
+    //Clear the security permissions.
+    security.permissions.clear();
+    //Save and dispose the document.
+    List<int> bytes = await document.save();
+    document.dispose();
+    //Open the PDF file.
+    _launchPdf(bytes, 'unsecured.pdf');
+  }
+
+  void _savePDF(String fileName, List<int> bytes) async {
+    final pdf = PdfDocument.fromBase64String(Base64Encoder().convert(bytes));
+    // On Flutter, use the [path_provider](https://pub.dev/packages/path_provider) library:
+    final output = await getTemporaryDirectory();
+    final file = File("${output.path}/$fileName");
+    print('The save file function is called');
+    // final file = File("example.pdf");
+    try {
+      await file.writeAsBytes(await pdf.save());
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,47 +112,63 @@ class MyApp extends StatelessWidget {
                 Text('About Me', style: TextStyle(fontWeight: FontWeight.w500)),
             centerTitle: true,
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CircleAvatar(
-                radius: 80.0,
-                backgroundImage: AssetImage('images/bigme.JPG'),
-              ),
-              Card(
-                color: Colors.grey.shade600,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text('Flutter Developer')),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FlatButton(
-                    color: Colors.red,
-                    onPressed: () {
-                      print('the yes button was pressed');
-                    },
-                    child: Text(
-                      'Yes',
-                      style: TextStyle(color: Colors.white),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: AssetImage('assets/bigme.JPG'),
+                ),
+                Card(
+                  color: Colors.grey.shade600,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text('Flutter Developer')),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FlatButton(
+                      color: Colors.red,
+                      onPressed: () {
+                        print('the yes button was pressed');
+                      },
+                      child: Text(
+                        'Yes',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
-                  FlatButton(
-                    color: Colors.blue,
-                    onPressed: () {
-                      print('the no button was pressed');
-                    },
-                    child: Text(
-                      'NO',
-                      style: TextStyle(color: Colors.white),
+                    FlatButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        print('the no button was pressed');
+                      },
+                      child: Text(
+                        'NO',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: securePdf,
+                        child: Text('Encrypt PDF'),
+                      ),
+                      // FlatButton(onPressed: securePdf, child: Text('Encrypt PDF')),
+                      // FlatButton(
+                      //     onPressed: restrictPermissions,
+                      //     child: Text('Restrict Permissions')),
+                      FlatButton(
+                          onPressed: decryptPDF, child: Text('Decrypt PDF')),
+                    ]),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/pdf_export.dart
@@ -0,0 +1,173 @@
+import 'dart:typed_data';
+
+import 'package:pdf/widgets.dart' as pw;
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<Uint8List> makePdf() async {
+  final pdf = pw.Document();
+  final imageLogo = pw.MemoryImage((await rootBundle.load(
+    'assets/bigme.JPG',
+  ))
+      .buffer
+      .asUint8List());
+  pdf.addPage(pw.Page(build: (context) {
+    return pw.Padding(
+      padding: const pw.EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+      child: pw.Column(
+        children: [
+          pw.Row(
+            children: [
+              pw.Image(imageLogo, height: 90.0, width: 120.0),
+              pw.SizedBox(width: 5.0),
+              pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Text('MediCard Davao Clinic',
+                      style: pw.TextStyle(
+                          fontSize: 20.0, fontWeight: pw.FontWeight.bold)),
+                  pw.Container(
+                    width: double.infinity - 25.0,
+                    child: pw.Text(
+                        'Unit 1, 8B-8C, LSA Center, J.P. Laurel Avenue, Bajada, Davao City,\nDavao City, Philippines\ninquiry@medicardphils.com, +639989631306',
+                        softWrap: true,
+                        maxLines: 3,
+                        style: pw.TextStyle(fontSize: 10.0)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          pw.Divider(),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: [
+              pw.Text('Name: ', style: pw.Theme.of(context).header4),
+              pw.Text('Date: ', style: pw.Theme.of(context).header4)
+            ],
+          ),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: [
+              pw.Text('Age:', style: pw.Theme.of(context).header5),
+              pw.Text('Sex: ', style: pw.Theme.of(context).header5)
+            ],
+          ),
+          pw.Text('Company: AMCOR SPECIALTY CARTONS RIZAL CORPS',
+              style: pw.Theme.of(context).header5),
+          pw.Text('Company: AMCOR SPECIALTY CARTONS RIZAL CORPS',
+              style: pw.Theme.of(context).header5),
+          pw.Divider(),
+          pw.Padding(
+              padding: const pw.EdgeInsets.fromLTRB(15.0, 5.0, 0.0, 10.0),
+              child: pw.Text(
+                  '1. 0.9 SODIUM CHLORIDE bb nn, #m m m\n(,, ,)\nSig: m m m as needed\nNotes: ,,',
+                  style: pw.Theme.of(context).header5)),
+          pw.SizedBox(
+            height: 80.0,
+          ),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: [
+              pw.Text('Next Visit: ________________________',
+                  style: pw.Theme.of(context).header5),
+              pw.Text('MEEKO DIASANTA', style: pw.Theme.of(context).header5),
+            ],
+          ),
+          pw.Text('License No: ________________________',
+              style: pw.Theme.of(context).header5),
+          pw.Text('PTR No: ________________________',
+              style: pw.Theme.of(context).header5),
+          pw.Text(
+              'This is a computer-generated medical record and has been electronically validated. If issued without any alteration, no signature is required.',
+              style: pw.Theme.of(context).header5),
+        ],
+      ),
+    );
+  }));
+  return pdf.save();
+}
+
+
+
+/*Scaffold(
+        appBar: kAppBar(context, 'My Cure PDF Viewer'),
+        body: LayoutBuilder(builder: (context, constraints) {
+          return Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/ic_logo_vertical.png',
+                      height: constraints.maxHeight * 0.1,
+                      width: constraints.maxWidth * 0.2,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('MediCard Davao Clinic',
+                            style: Theme.of(context).textTheme.headline6),
+                        Text(
+                            'Unit 1, 8B-8C, LSA Center, J.P. Laurel Avenue, Bajada, Davao City,\nDavao City, Philippines\ninquiry@medicardphils.com, +639989631306',
+                            style: Theme.of(context).textTheme.bodyText2),
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Name: $name',
+                        style: Theme.of(context).textTheme.bodyText2),
+                    Text('Date: $date',
+                        style: Theme.of(context).textTheme.bodyText2)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Age: $age',
+                        style: Theme.of(context).textTheme.bodyText2),
+                    Text('Sex: $sex',
+                        style: Theme.of(context).textTheme.bodyText2)
+                  ],
+                ),
+                Text('Company: AMCOR SPECIALTY CARTONS RIZAL CORPS',
+                    style: Theme.of(context).textTheme.bodyText2),
+                Text('Company: AMCOR SPECIALTY CARTONS RIZAL CORPS',
+                    style: Theme.of(context).textTheme.bodyText2),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 5.0, 0.0, 10.0),
+                  child: Text(
+                      '1. 0.9 SODIUM CHLORIDE bb nn, #m m m\n(,, ,)\nSig: m m m as needed\nNotes: ,,',
+                      style: Theme.of(context).textTheme.headline6),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Next Visit: ________________________',
+                        style: Theme.of(context).textTheme.bodyText2),
+                    Text('MEEKO DIASANTA',
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ],
+                ),
+                Text('License No: ________________________',
+                    style: Theme.of(context).textTheme.bodyText2),
+                Text('PTR No: ________________________',
+                    style: Theme.of(context).textTheme.bodyText2),
+                Text(
+                    'This is a computer-generated medical record and has been electronically validated. If issued without any alteration, no signature is required.',
+                    style: Theme.of(context).textTheme.bodyText2),
+              ],
+            ),
+          );
+        }))*/
